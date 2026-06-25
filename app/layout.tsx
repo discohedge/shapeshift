fc2f487fa3983d6856bab276c86584d31bfda3c3
shapeshift <157481850+discohedge@users.noreply.github.com>
2026-06-25 12:22:32 -0700

    Add Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Cursor from "./Cursor";
 import { GeistSans } from "geist/font/sans";
 import { GeistPixelGrid } from "geist/font/pixel";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -10,6 +11,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistPixelGrid.variable}`}>
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-6P20RD4T76"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6P20RD4T76');
+          `}
+        </Script>
+      </head>
       <body className="font-sans">
         <Cursor />
         {children}
